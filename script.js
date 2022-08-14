052ac54b9d0628694e37270e936712a26c0b053b
Alejandra Lee Vargas <alejandraleevargas@Alejandras-MacBook-Pro.local>
2022-08-14 15:55:27 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Ejercicio 1. Cambia el color de fondo del elemento body a "#DDD894"
+
+
+/*
+Ejercicio 2. Utiliza el método .querySelector() para:
+Crear una variable "bird" con el valor del elemento con clase ".bird"
+Crear una variable "sky" el elemento con clase ".sky"
+Crear una variable "ground" el elemento con clase ".ground"
+*/
+
+let bird
+let sky = document.querySelector(".sky")
+let ground
+
+
+// Ejercicio 3. Utiliza console.log() para imprimir en la consola el valor de bird, sky, y ground
+console.log(sky)
+
+
+
+/*
+Ejercicio 4. Utiliza la propiedad style para que:
+La propiedad bottom de bird tenga un valor de 300px
+La propiedad left de bird tenga un valor de 500px 
+*/
+
+bird.style.bottom= '10px';
