8b679fd423742b4bb62b5fe8e5aeca7f11b3ef29
cesar1215 <cenriquez@angloaleman.edu.mx>
2022-08-16 08:05:05 -0500

    Ejercicios Javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Ejercicio 1. Cambia el color de fondo del elemento body a "#DDD894"
-
+document.body.style.backgroundColor = "#DDD894"
 
 /*
 Ejercicio 2. Utiliza el método .querySelector() para:
@@ -8,13 +8,15 @@ Crear una variable "sky" el elemento con clase ".sky"
 Crear una variable "ground" el elemento con clase ".ground"
 */
 
-let bird
+let bird = document.querySelector(".bird")
 let sky = document.querySelector(".sky")
-let ground
+let ground = document.querySelector(".ground")
 
 
 // Ejercicio 3. Utiliza console.log() para imprimir en la consola el valor de bird, sky, y ground
 console.log(sky)
+console.log(bird)
+console.log(ground)
 
 
 
@@ -24,4 +26,5 @@ La propiedad bottom de bird tenga un valor de 300px
 La propiedad left de bird tenga un valor de 500px 
 */
 
-bird.style.bottom= '10px';
+bird.style.bottom= '300px';
+bird.style.left= '500px';
